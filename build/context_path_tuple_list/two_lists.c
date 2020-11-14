0fe1f2f64cd022bf834174a123c7b23b3b5af4be
Kerem Çakırer <kcakirer@hotmail.com>
2020-11-14 22:41:10 +0100

    Update webpack

--- build/context_path_tuple_list/two_lists.c
+++
@@ -1,8 +0,0 @@
-int g(int i) {
-    return 51;
-}int main(void) {
-    int x = 13;
-    g(5);
-    x++;
-    g(17);
-}
--- src/Datafetcher.ml
+++ src/Datafetcher.ml
@@ -1,9 +1,10 @@
 open Js_of_ocaml
 open Js_of_ocaml_lwt
 open Lwt
+
 (* open Js *)
 
-let base_url = "http://localhost:8000/"
+let base_url = "/"
 
 let http_get url =
   XmlHttpRequest.get url
@@ -12,7 +13,8 @@ let http_get url =
   let msg = r.XmlHttpRequest.content in
   if cod = 0 || cod = 200 then Lwt.return msg else fst (Lwt.wait ())
 
-let http_get_with_base filename = http_get @@ base_url^filename
+let http_get_with_base filename = http_get @@ base_url ^ filename
 
 let get_file f =
-  try Lwt.return (Sys_js.read_file ~name:f) with Not_found -> http_get f >|= fun s -> s
+  try Lwt.return (Sys_js.read_file ~name:f)
+  with Not_found -> http_get f >|= fun s -> s
