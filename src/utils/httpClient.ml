490b31b83cba35b6c9dc072e8d8fbd805638bf2d
Kerem Çakırer <kcakirer@hotmail.com>
2021-04-13 04:10:54 +0300

    Add on_get helper function to HttpClient

--- src/utils/httpClient.ml
+++ src/utils/httpClient.ml
@@ -1,3 +1,4 @@
+open Batteries
 open Js_of_ocaml
 open Js_of_ocaml_lwt
 open Lwt
@@ -5,8 +6,7 @@ open Lwt
 type error = HttpFailure of int | NoContentAvailable
 
 let get url =
-  XmlHttpRequest.perform_raw ~response_type:XmlHttpRequest.ArrayBuffer url
-  >>= fun r ->
+  XmlHttpRequest.perform_raw ~response_type:XmlHttpRequest.ArrayBuffer url >>= fun r ->
   let code = r.code in
   if code < 200 || code >= 400 then Lwt.return (Error (HttpFailure code))
   else
@@ -15,3 +15,9 @@ let get url =
       (fun c -> Lwt.return (Ok (Typed_array.String.of_arrayBuffer c)))
 
 let get_opt url = get url >>= fun resp -> Lwt.return (Result.to_option resp)
+
+let on_get f url =
+  Lwt.bind (get url) (fun res ->
+      f res;
+      Lwt.return_unit)
+  |> ignore
