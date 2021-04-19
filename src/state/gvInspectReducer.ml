bb9e44e60337df0d6fbdf0d91b6342a795797472
Kerem Çakırer <kcakirer@hotmail.com>
2021-04-19 16:28:13 +0300

    Remove unit arg in GvInspect.line & node

--- src/state/gvInspectReducer.ml
+++ src/state/gvInspectReducer.ml
@@ -2,5 +2,5 @@ open Batteries
 module I = GvInspect
 
 let reduce (_ : State.t) (_ : I.t option) = function
-  | `InspectLine (file, num) -> I.line ~file ~num () |> Option.some
-  | `InspectNode id -> I.node ~id () |> Option.some
+  | `InspectLine (file, num) -> I.line ~file ~num |> Option.some
+  | `InspectNode id -> I.node ~id |> Option.some
