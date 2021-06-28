e6a852b6436b77202455e55255373e52e2c81b9c
Kerem Çakırer <kcakirer@hotmail.com>
2021-06-29 01:13:11 +0200

    Create stubs for some Unix functions Goblint uses

---
+++ runtime/stubs.js
@@ -0,0 +1,12 @@
+//Provides: unix_getpid
+function unix_getpid() {
+    return 0;
+}
+
+//Provides: unix_kill
+function unix_kill(pid, signal) { }
+
+//Provides: unix_setitimer
+function unix_setitimer(which, newval) {
+    return newval;
+}
