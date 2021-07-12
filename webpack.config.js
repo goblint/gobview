68db5c8932c13b5e6395693a984058cdf23aa328
Kerem Çakırer <kcakirer@hotmail.com>
2021-07-13 00:31:34 +0200

    Upgrade to webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,28 @@
 const path = require('path');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProd = process.env.NODE_ENV === 'production';
-
 module.exports = {
-  entry: './_build/default/src/App.bc.js',
-  mode: isProd ? 'production' : 'development',
-  node: {
-    fs: 'empty',
-    child_process: 'empty'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.resolve(__dirname, 'public')
-  }
+    mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
+    entry: './_build/default/src/App.bc.js',
+    module: {
+        rules: [
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: { postcssOptions: { plugins: ['autoprefixer'] } }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: 'public/index.html' })],
+    resolve: { fallback: { child_process: false, constants: false, fs: false, tty: false } },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    }
 };
