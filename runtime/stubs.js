55f1de6b1493e6fde3d717469a5c56616ce27b27
Kerem Çakırer <kcakirer@hotmail.com>
2021-10-02 02:23:46 +0300

    Stubs for Apron functions

--- runtime/stubs.js
+++ runtime/stubs.js
@@ -16,3 +16,24 @@ function ml_z_mul_overflows(x) {
     // This is necessary until https://github.com/janestreet/zarith_stubs_js/pull/8 is fixed.
     return true;
 }
+
+//Provides: camlidl_apron_init
+function camlidl_apron_init() { return 0; }
+
+//Provides: camlidl_apron_set_var_operations
+function camlidl_apron_set_var_operations() { return 0; }
+
+//Provides: camlidl_oct_oct_manager_alloc
+function camlidl_oct_oct_manager_alloc() { return 0; }
+
+//Provides: camlidl_var_ap_var_of_string
+function camlidl_var_ap_var_of_string() { return 0; }
+
+//Provides: camlidl_environment_ap_environment_make
+function camlidl_environment_ap_environment_make() { return 0; }
+
+//Provides: camlidl_abstract1_ap_abstract1_top
+function camlidl_abstract1_ap_abstract1_top() { return 0; }
+
+//Provides: camlidl_var_ap_var_hash
+function camlidl_var_ap_var_hash() { return 0; }
