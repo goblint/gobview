020afb2363865e5d796f5f531042da393d3e95d4
Kerem Çakırer <kcakirer@hotmail.com>
2021-11-01 19:33:14 +0100

    Update build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
-    entry: './_build/default/src/App.bc.js',
+    entry: './src/App.bc.js',
     module: {
         rules: [
             {
