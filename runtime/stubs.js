e95cdde64655d09b7dc48d2274bb3b009a317121
Simmo Saan <simmo.saan@gmail.com>
2021-11-05 15:22:39 +0200

    Add Apron box and polka alloc stubs

--- runtime/stubs.js
+++ runtime/stubs.js
@@ -26,6 +26,12 @@ function camlidl_apron_set_var_operations() { return 0; }
 //Provides: camlidl_oct_oct_manager_alloc
 function camlidl_oct_oct_manager_alloc() { return 0; }
 
+//Provides: camlidl_polka_pk_manager_alloc_loose
+function camlidl_polka_pk_manager_alloc_loose() { return 0; }
+
+//Provides: camlidl_box_box_manager_alloc
+function camlidl_box_box_manager_alloc() { return 0; }
+
 //Provides: camlidl_var_ap_var_of_string
 function camlidl_var_ap_var_of_string() { return 0; }
 
