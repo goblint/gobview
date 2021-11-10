bb918e8264944b744f6c31ed13e591ecd1bffb1d
Simmo Saan <simmo.saan@gmail.com>
2021-11-10 11:52:20 +0200

    Add stubs for sha library

---
+++ runtime/sha_stubs.js
@@ -0,0 +1,26 @@
+//Provides: stub_sha512_init
+function stub_sha512_init() { return 0; }
+
+//Provides: stub_sha512_update
+function stub_sha512_update() { return 0; }
+
+//Provides: stub_sha512_finalize
+function stub_sha512_finalize() { return 0; }
+
+//Provides: stub_sha256_init
+function stub_sha256_init() { return 0; }
+
+//Provides: stub_sha256_update
+function stub_sha256_update() { return 0; }
+
+//Provides: stub_sha256_finalize
+function stub_sha256_finalize() { return 0; }
+
+//Provides: stub_sha1_init
+function stub_sha1_init() { return 0; }
+
+//Provides: stub_sha1_update
+function stub_sha1_update() { return 0; }
+
+//Provides: stub_sha1_finalize
+function stub_sha1_finalize() { return 0; }
