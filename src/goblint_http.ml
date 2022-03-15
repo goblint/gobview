b75bc99162be2eacb877d788f14e57c311ff4741
Kerem Çakırer <kcakirer@hotmail.com>
2022-03-15 11:14:53 +0100

    Run ocamlformat

--- src/goblint_http.ml
+++ src/goblint_http.ml
@@ -5,12 +5,15 @@ let port = ref 8080
 let goblint = ref "goblint"
 let rest = ref []
 
-let specs = [
-  ("-addr", Arg.Set_string addr, "Listen address");
-  ("-port", Arg.Set_int port, "Listen port");
-  ("-with-goblint", Arg.Set_string goblint, "Path to the Goblint executable");
-  ("-goblint", Arg.Rest_all (fun args -> rest := args), "Pass the rest of the arguments to Goblint")
-]
+let specs =
+  [
+    ("-addr", Arg.Set_string addr, "Listen address");
+    ("-port", Arg.Set_int port, "Listen port");
+    ("-with-goblint", Arg.Set_string goblint, "Path to the Goblint executable");
+    ( "-goblint",
+      Arg.Rest_all (fun args -> rest := args),
+      "Pass the rest of the arguments to Goblint" );
+  ]
 
 let paths = ref []
 
