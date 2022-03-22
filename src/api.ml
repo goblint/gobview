0a7c3d678fe4236868d2f840c498012c674b832d
Kerem Çakırer <kcakirer@hotmail.com>
2022-03-22 13:18:38 +0300

    JSON API

---
+++ src/api.ml
@@ -0,0 +1,26 @@
+open Batteries
+
+module type Request = sig
+  val name: string
+
+  type body
+  type response
+
+  val body_of_yojson: Yojson.Safe.t -> body
+  val yojson_of_response: response -> Yojson.Safe.t
+
+  val process: body -> response
+end
+
+let registry = Hashtbl.create 16
+
+let register (module R : Request) = Hashtbl.add registry R.name (module R : Request)
+
+module Ping = struct
+  let name = "ping"
+  type body = unit [@@deriving yojson]
+  type response = unit [@@deriving yojson]
+  let process () = ()
+end
+
+let () = register (module Ping)
