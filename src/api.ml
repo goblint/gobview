03aea71a18a67180d93af7b61e8ad46c02ba60e8
Kerem Çakırer <kcakirer@hotmail.com>
2022-03-22 14:25:05 +0300

    Handle JSON API requests

--- src/api.ml
+++ src/api.ml
@@ -9,7 +9,7 @@ module type Request = sig
   val body_of_yojson: Yojson.Safe.t -> body
   val yojson_of_response: response -> Yojson.Safe.t
 
-  val process: body -> response
+  val process: body -> response Lwt.t
 end
 
 let registry = Hashtbl.create 16
@@ -20,7 +20,7 @@ module Ping = struct
   let name = "ping"
   type body = unit [@@deriving yojson]
   type response = unit [@@deriving yojson]
-  let process () = ()
+  let process () = Lwt.return_unit
 end
 
 let () = register (module Ping)
--- src/goblint_http.ml
+++ src/goblint_http.ml
@@ -1,5 +1,9 @@
 open Batteries
+open Cohttp_lwt
 open Cohttp_lwt_unix
+open Lwt.Infix
+
+module Yojson_conv = Ppx_yojson_conv_lib.Yojson_conv
 
 let addr = ref "127.0.0.1"
 let port = ref 8080
@@ -11,17 +15,38 @@ let specs =
     ("-addr", Arg.Set_string addr, "Listen address");
     ("-port", Arg.Set_int port, "Listen port");
     ("-with-goblint", Arg.Set_string goblint, "Path to the Goblint executable");
-    ( "-goblint",
-      Arg.Rest_all (fun args -> rest := args),
-      "Pass the rest of the arguments to Goblint" );
+    ("-goblint", Arg.Rest_all (fun args -> rest := args), "Pass the rest of the arguments to Goblint");
   ]
 
 let paths = ref []
 
+let process name body =
+  match Hashtbl.find_option Api.registry name with
+  | None -> Server.respond_not_found ()
+  | Some (module R) ->
+    let%lwt body = Body.to_string body in
+    match Yojson.Safe.from_string body with
+    | exception Yojson.Json_error err -> Server.respond_error ~status:`Bad_request ~body:err ()
+    | json ->
+      match R.body_of_yojson json with
+      | exception Yojson_conv.Of_yojson_error (exn, _) ->
+        Server.respond_error ~status:`Bad_request ~body:(Printexc.to_string exn) ()
+      | body ->
+        let response = R.process body >|= R.yojson_of_response >|= Yojson.Safe.to_string in
+        response >>= fun body -> Server.respond_string ~status:`OK ~body ()
+
+let callback _ req body =
+  let uri = Request.uri req in
+  let path = Uri.path uri in
+  let parts = String.split_on_char '/' path |> List.filter (not % String.is_empty) in
+  let meth = Request.meth req in
+  match meth, parts with
+  | `POST, ["api"; name] -> process name body
+  | `GET, _ -> Server.respond_not_found () (* TODO: Forward Goblint state *)
+  | _ -> Server.respond_not_found ()
+
 let main () =
-  let%lwt gob = Goblint.spawn !goblint (!rest @ !paths) in
-  let router = Routes.router gob in
-  let callback _ req _ = Router.route router req |> Option.default (Server.respond_not_found ()) in
+  let%lwt _ = Goblint.spawn !goblint (!rest @ !paths) in
   let server = Server.make ~callback () in
   Server.create ~mode:(`TCP (`Port !port)) server
 
