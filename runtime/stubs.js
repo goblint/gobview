a386a0d6e34c10e5fafbb491a820501985d1bea5
stilscher <66023521+stilscher@users.noreply.github.com>
2023-02-07 14:47:24 +0100

    provide missing stubs

--- runtime/stubs.js
+++ runtime/stubs.js
@@ -17,6 +17,32 @@ function ml_z_mul_overflows(x) {
     return true;
 }
 
+//Provides: unix_times
+function unix_times(x) {
+  return x;
+}
+
+//Provides: max_float
+function max_float(x) {
+  return x;
+}
+
+//Provides: smallest_float
+function smallest_float(x) {
+  return x;
+}
+
+//Provides: ml_z_of_int
+function ml_z_of_int(x) {
+    return x | 0;
+}
+
+//Provides: caml_thread_initialize const
+function caml_thread_initialize() {}
+
+//Provides: caml_mutex_new const
+function caml_mutex_new() {}
+
 //Provides: camlidl_apron_init
 function camlidl_apron_init() { return 0; }
 
@@ -28,6 +54,9 @@ function camlidl_oct_oct_manager_alloc() { return 0; }
 
 //Provides: camlidl_polka_pk_manager_alloc_loose
 function camlidl_polka_pk_manager_alloc_loose() { return 0; }
+
+//Provides: camlidl_polka_pk_manager_alloc_equalities
+function camlidl_polka_pk_manager_alloc_equalities() { return 0; }
 
 //Provides: camlidl_box_box_manager_alloc
 function camlidl_box_box_manager_alloc() { return 0; }
@@ -43,3 +72,9 @@ function camlidl_abstract1_ap_abstract1_top() { return 0; }
 
 //Provides: camlidl_var_ap_var_hash
 function camlidl_var_ap_var_hash() { return 0; }
+
+//Provides: camlidl_mpq_mpq_init
+function camlidl_mpq_mpq_init() { return 0; }
+
+//Provides: camlidl_mpq_mpq_set_si
+function camlidl_mpq_mpq_set_si() { return 0; }
