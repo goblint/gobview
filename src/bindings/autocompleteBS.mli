b190c66dba3fa9993f1f7900379109f184eace58
sxprz <derian.pljakic@gmail.com>
2023-07-13 15:02:06 +0200

    Clean unused deps

--- src/bindings/autocompleteBS.mli
+++
@@ -1,8 +0,0 @@
-open Js_of_ocaml
-
-type options
-val options : unit -> options
-
-val autocompleteBS : 
-  options ->
-  (Dom_html.inputElement Js.t -> unit) [@@js.global "autocompleteBS"]
--- src/static-requires.js
+++ src/static-requires.js
@@ -6,6 +6,3 @@ require('./stylesheet.scss');
 joo_global_object.monaco = require('monaco-editor/esm/vs/editor/editor.api');
 require('monaco-editor/esm/vs/basic-languages/cpp/cpp.contribution');
 require('monaco-editor/esm/vs/editor/contrib/hover/hover');
-
-var autocompleteBS = require("bootstrap5-autocomplete");
-joo_global_object.autocompleteBS = autocompleteBS;
